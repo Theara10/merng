ce8f6d14309d32d1a127fe420064b53ed7b217b4
Theara10 <thearatheng10@gmail.com>
2021-02-13 08:06:17 +0700

    add like post

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,4 +1,5 @@
-const { AuthenticationError } = require("apollo-server");
+const { AuthenticationError, UserInputError } = require("apollo-server");
+const { argsToArgsConfig } = require("graphql/type/definition");
 const Post = require("../../models/Post");
 const checkAuth = require("../../util/check-auth");
 
@@ -31,6 +32,10 @@ module.exports = {
     async createPost(_, { body }, context) {
       const user = checkAuth(context);
 
+      if (args.body.trim() === "") {
+        throw new Error("Post body must not be empty");
+      }
+
       const newPost = new Post({
         body,
         user: user.id,
@@ -56,6 +61,28 @@ module.exports = {
         }
       } catch (err) {
         throw new Error(err);
+      }
+    },
+
+    async likePost(_, { postId }, context) {
+      const { username } = checkAuth(context);
+
+      const post = await Post.findById(postId);
+      if (post) {
+        if (post.likes.find((like) => like.username === username)) {
+          // post alread liked, uklike it
+          post.likes = post.likes.filter((like) => like.username !== username);
+        } else {
+          // not liked, like it
+          post.likes.push({
+            username,
+            createdAt: new Date().toISOString(),
+          });
+        }
+        await post.save();
+        return post;
+      } else {
+        throw new UserInputError("Post not found");
       }
     },
   },
